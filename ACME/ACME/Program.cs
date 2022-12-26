e400198e4f3fe9e48af766b4685da186734f7e41
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2022-12-26 11:13:20 -0400

    Add files via upload

---
+++ ACME/ACME/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Acme Accounting Systems");
+Console.WriteLine("Remember, we're \"accounting\" on you!");
