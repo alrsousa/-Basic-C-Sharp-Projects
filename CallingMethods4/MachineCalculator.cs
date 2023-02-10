b72bb08ecdc822bd42bda178358878dbe6f1a20d
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-02-10 11:30:44 -0400

    Add files via upload

---
+++ CallingMethods4/MachineCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace CallingMethods4
+{
+    public class MachineCalculator
+    {
+        public void ResultMethod(int number1, int number2)
+        {
+            int result = number1 * number2;
+            Console.WriteLine("Multiplying your numbers, we have the result ****" + result + "****");
+        }
+
+    }
+}
---
+++ CallingMethods4/Program.cs
@@ -0,0 +1,32 @@
+﻿using CallingMethods4;
+using System;
+
+namespace Calling_Methods4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number:     ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please choose a second number:     ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            MachineCalculator FirstOp = new MachineCalculator();
+            FirstOp.ResultMethod(number1, number2);
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** The program has ended *****");
+            Console.WriteLine("********************************");
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** Thank you for your attention! *****");
+            Console.WriteLine("********************************");
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
