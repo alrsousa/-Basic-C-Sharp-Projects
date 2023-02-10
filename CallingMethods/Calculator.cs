2f720a27c93c2d5f20c82db79753a6d5ca9bd41d
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-02-09 22:03:04 -0400

    Add files via upload

---
+++ CallingMethods/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CS_Class_Methods
+{
+    class Calculator  //Class defined to calculate output of methods
+    {
+
+        public static int Mult(int numb)  //Method to Multiply by 50 the inputed number
+        {
+            return numb * 50;
+        }
+
+        public static int Add(int numb)  //Method to add 500 to inputed number
+        {
+            return numb + 500;
+        }
+ 
+        public static int Subtract(int numb) //Method to deduct 100 from inputed number
+        {
+            return numb - 100;
+        }
+    }
+}
---
+++ CallingMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using CS_Class_Methods;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number to do 3 math functions with it:   ");
+            int numb = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(numb + " + 500 *** EQUALS *** " + Calculator.Add(numb));
+            Console.WriteLine(numb + " x 50  *** EQUALS *** " + Calculator.Mult(numb));
+            Console.WriteLine(numb + " - 100 *** EQUALS *** " + Calculator.Subtract(numb));
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** The program has ended *****");
+            Console.WriteLine("********************************");
+            Console.ReadLine();
+        }
+    }
+}
+
