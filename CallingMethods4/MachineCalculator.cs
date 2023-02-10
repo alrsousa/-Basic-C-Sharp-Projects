28273facb632f7d8fe7beaf416be6696461cdc14
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-02-10 15:30:37 -0400

    Add files via upload

---
+++ CallingMethods4/MachineCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace CallingMethods4
+{
+    public class MachineCalculator
+    {
+        public void ResultMethod(int parameter1, int parameter2)
+        {
+            int result = parameter1 * parameter2;
+            Console.WriteLine("Multiplying your numbers, we have the result ****" + result + "****");
+        }
+
+    }
+}
