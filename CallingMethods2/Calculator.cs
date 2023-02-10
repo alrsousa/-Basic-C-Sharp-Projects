f320cbc1665e5a87396f1f7655dd7cc549bcd450
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-02-09 22:36:50 -0400

    Add files via upload

---
+++ CallingMethods2/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculadora
+    {
+        public int MathMethod(int num1)
+        {
+            int num1Result = num1 + 10;
+            return num1Result;
+        }
+
+        public decimal MathMethod(int num1, decimal num2)
+        {
+            decimal num2Result = num2 * 10;
+            return num2Result;
+        }
+
+        public int MathMethod(int num1, decimal num2, string num3)
+        {
+            int num4 = Convert.ToInt32(num3);
+            int num3Result = num4 - 10;
+            return num3Result;
+        }
+    }
+}
---
+++ CallingMethods2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Calculator;
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            decimal num2 = 70.69m;
+            string str1 = "70";
+            int num3 = Int32.Parse(str1);
+
+            Calculadora FirstOp = new Calculadora();
+            Calculadora SecondOp = new Calculadora();
+            Calculadora ThirdOp = new Calculadora();
+            Console.WriteLine(FirstOp.MathMethod(10));
+            Console.WriteLine(SecondOp.MathMethod(10, 70.69m));
+            Console.WriteLine(ThirdOp.MathMethod(10, 70.69m, str1));
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** The program has ended *****");
+            Console.WriteLine("********************************");
+            Console.ReadLine();
+
+        }
+    }
+}
