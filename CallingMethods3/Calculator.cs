f96ddbe757e407dbe7b7bac3d1d11e9d6a44f9cc
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-02-09 23:47:28 -0400

    Add files via upload

---
+++ CallingMethods3/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CS_CallingMethods3
+{
+    public class Calculator    {
+        public int MathMethod(int num1, string ans)
+        {
+            int result = 0;
+            if (ans == "") //When user didn´t typed the second Number
+            {
+                result = num1 + 10;
+
+            }
+            else //Calculate using the 2 inputed numbers + 10
+            {
+                int num2 = Convert.ToInt32(ans);
+                result = num1 + num2 + 10;
+            }
+            return result;
+
+        }
+
+    }
+}
---
+++ CallingMethods3/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using CS_CallingMethods3;
+using System;
+
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:   ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number if you want. Otherwise just press Enter.   ");
+            string ans = Console.ReadLine();
+
+            Calculator FirstOp = new Calculator(); //Create the object from class Calculator
+
+            Console.WriteLine("Your input(s) + 10 = " + FirstOp.MathMethod(num1, ans));
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** The program has ended *****");
+            Console.WriteLine("********************************");
+
+            Console.ReadLine();
+
+        }
+    }
+}
