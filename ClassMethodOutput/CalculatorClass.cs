a67e8480cc2de45a71e120765e2f6bde59a16a3c
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-02-10 14:42:20 -0400

    Add files via upload

---
+++ ClassMethodOutput/CalculatorClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMethodOutputAssigment
+{
+    class Calculator  //Class used to calculate values with distinct DataTypes
+    {
+        public int GetDiv(int data)  //Calculating for integer input
+        {
+            return data / 2;
+        }
+
+        public int GetDiv(decimal data)  //Calculating for decimal input after converted to integer
+        {
+            int intNum = Convert.ToInt32(data);
+            return intNum / 2;
+        }
+
+        public bool GetDiv(string data, out int stgNum)
+        {
+            if (int.TryParse(data, out int intNum))
+            {
+                stgNum = intNum / 2;
+                return true;
+            }
+            else
+            {
+                stgNum = 0;
+                return false;
+            }
+
+        }
+
+    }
+
+}
+
---
+++ ClassMethodOutput/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMethodOutputAssigment
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.Write("Please type a number:   ");
+            string line = Console.ReadLine();
+            var operatorObject = new Calculator();
+
+            if (int.TryParse(line, out int Num))
+            {
+                Console.WriteLine("Your INTEGER number divided by 2 is EQUAL ****  " + operatorObject.GetDiv(Num));
+            }
+            else if (decimal.TryParse(line, out decimal Dec))
+            {
+                Console.WriteLine("Your DECIMAL number divided by 2 is EQUAL ****   " + operatorObject.GetDiv(Dec));
+            }
+            else if (operatorObject.GetDiv(line, out int stgNum))
+            {
+                Console.WriteLine(stgNum);
+            }
+            else
+            {
+                Console.WriteLine("Your input is invalid!!!");
+            }
+
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** The program has ended *****");
+            Console.WriteLine("********************************");
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+
+
+}
