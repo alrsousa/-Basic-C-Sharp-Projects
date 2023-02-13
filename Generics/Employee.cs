c3a5e5c44b46c848d2209de0bb75bab560dd3057
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-02-13 11:09:47 -0400

    Add files via upload

---
+++ Generics/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ParametersAssignment
+{
+    public class Employee<T>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public Employee(int id, string fn, string ln)
+        {
+            id = ID;
+            fn = FirstName;
+            ln = LastName;
+        }
+        public List<T> things { get; set; }  //Using System.Collections.Generic
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> emp1 = new Employee<string>(1, "Ricardo", "Tarantino"); //Adding values to Employee collection emp1
+            emp1.things = new List<string>() { "Ricardo", "Tarantino", "has been", "hired." };
+            emp1.things.ForEach(i => Console.Write("{0} ", i));  //print colletion to console
+            Console.ReadLine();
+
+            Employee<int> emp2 = new Employee<int>(2, "Juan Pablo", "Montoya"); //Adding values to Employee collection emp2
+            emp2.things = new List<int>() { 22, 35, 14, 47 };
+            emp2.things.ForEach(i => Console.Write("{0} \n", i));  //print colletion to console
+            Console.ReadLine();
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** The program has ended *****");
+            Console.WriteLine("********************************");
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** Thank you for your attention! *****");
+            Console.WriteLine("********************************");
+
+
+        }
+    }
+}
