cb22aeb16bfa08373a31b330459c610812cd4210
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-02-13 10:27:23 -0400

    Add files via upload

---
+++ OperatorAssignment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace OperatorsAssignment
+{
+    public class Employee//public class Employee
+    {
+        public string Id { get; set; }//Id property
+        
+        public string FirstName { get; set; }//FirstName property
+        public string LastName { get; set; }//LastName property
+
+        public static bool operator == (Employee employee1, Employee employee2)//overloaded bool operator '==' taking in employee1 and employee2
+        {
+            Console.WriteLine("This is comparing employee1 and employee2 Id property, and displaying 'true' if they equal each other, and false if the Id property values do not equal each other");
+            Console.WriteLine("");
+            return employee1.Id == employee2.Id;//returns bool value of employee1 id and employee2 id (true or false)
+        }
+        public static bool operator!= (Employee employee1, Employee employee2)//overloaded bool operator '!=' taking in employee1 and employee2
+        {
+            Console.WriteLine("This is comparing employee1 and employee2 Id property, and displaying 'true' if they do not equal each other, and false if the Id property values do equal each other");
+            Console.WriteLine("");
+            return employee1.Id != employee2.Id;//returns bool value of employee1 id and employee2 id (true or false)
+        }
+
+    }
+}
---
+++ OperatorAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();//instantiate Employee object 'employee1'
+            employee1.Id = "1";//sets value of Id property to 1
+            employee1.FirstName = "James";//sets value of FirstName property to 'James'
+            employee1.LastName = "Hetfield";//sets value of Last Name property to 'Hetfield'
+
+            Employee employee2 = new Employee();//instantiates Employee object 'employee2'
+            employee2.Id = "2";//sets value of employee1.Id property to 2
+            employee2.FirstName = "Kirk";//sets value of FirstName property to 'Kirk'
+            employee2.LastName = "Hammet";//sets value of LastName property to 'Hammet'
+
+            Console.WriteLine(employee1 == employee2);//display to console bool value of 'is employee1 equal to employee2?'
+            Console.WriteLine(employee1 != employee2);//display to console bool value of 'is employee1 not equal to employee2?'
+
+
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** The program has ended *****");
+            Console.WriteLine("********************************");
+            Console.WriteLine("********************************");
+            Console.WriteLine("***** Thank you for your attention! *****");
+            Console.WriteLine("********************************");
+
+
+            Console.ReadLine();//ensures console remains open
+
+
+        }
+    }
+}
