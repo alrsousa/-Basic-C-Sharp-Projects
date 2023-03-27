650727251e2341fea8e6943ff6af286d07c22372
Andre Sousa <38532992+alrsousa@users.noreply.github.com>
2023-03-26 23:15:00 -0300

    Add files via upload

---
+++ FinalAssigmentStudentDB/StudentDb/StudentDB/Student.cs
@@ -0,0 +1,15 @@
+﻿
+namespace StudentDB
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+}
